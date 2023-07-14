ae5ae90db0c7c80bfa99d6950c1c2a24e22e8c53
leocode <guzmanmarcosguzman@outlook.com>
2023-07-13 23:57:30 -0600

    validacion de la fecha de nacimiento que sea mayor a 18 años

---
+++ main/validaciones.js
@@ -0,0 +1,22 @@
+const inputNcimiento = document.querySelector("#birth");
+
+inputNcimiento.addEventListener("blur", (evento) =>{
+    validarNcimiento(evento.target);
+});
+
+function validarNcimiento(input){
+    const fechaCliente = new Date(input.value);
+    let mensaje = "";
+    if(!mayorEdad(fechaCliente)){
+        mensaje = "Debes tener al menos 18 años de edad"
+    }
+    
+
+   input.setCustomValidity(mensaje);
+}
+
+function mayorEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(fecha.getUTCFullYear() + 18, fecha.getUTCMonth(), fecha.getUTCDate());
+    return diferenciaFechas <= fechaActual;
+}
