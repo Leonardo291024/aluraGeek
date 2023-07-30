ee59d7d402fff628ae51747c7c379c67878c33e3
leocode <guzmanmarcosguzman@outlook.com>
2023-07-30 13:52:46 -0600

    validacion de la seccion de enviar mensaje

--- main/app.js
+++ main/app.js
@@ -1,6 +1,13 @@
 import {valida} from "./validaciones.js";
 
 const inputs = document.querySelectorAll("input");
+const textAreas = document.querySelectorAll("textarea");
+
+textAreas.forEach((input) =>{
+    input.addEventListener("blur", (input) =>{
+        valida(input.target)
+    })
+})
 
 inputs.forEach((input) =>{
     input.addEventListener("blur", (input) =>{
