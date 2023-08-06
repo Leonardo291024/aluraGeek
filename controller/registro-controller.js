278452defd9898e1107236d9f63ce1d4ec5772d7
leocode <guzmanmarcosguzman@outlook.com>
2023-08-06 17:45:54 -0600

    registro de un cliente utilizando el metodo create con fetch

---
+++ controller/registro-controller.js
@@ -0,0 +1,18 @@
+import { clientServices } from "../service/client-service.js";
+
+const formulario = document.querySelector('[data-form]');
+
+formulario.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+
+    const nombre = document.querySelector('[data-nombre]').value;
+    const email = document.querySelector('[data-email]').value;
+    const contrasena = document.querySelector('[data-pasword]').value;
+
+    console.log(nombre, " ", email, " ", contrasena);
+
+    clientServices.crearCliente(nombre, email).then(() =>{
+        alert("REGISTRO COMPLETADO");
+        window.location.href = '/nuevoProducto.html';
+    }).catch((err) => console.log(err));
+});
